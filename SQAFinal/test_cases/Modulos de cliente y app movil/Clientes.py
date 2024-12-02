b33f4d05dbf5bfbc89203c4acfca274204ae92e7
Yamil222 <145218410+Yamil222@users.noreply.github.com>
2024-12-02 14:52:32 -0400

    Add files via upload

--- SQAFinal/test_cases/Modulos de cliente y app movil/Clientes.py
+++ SQAFinal/test_cases/Modulos de cliente y app movil/Clientes.py
@@ -1,38 +1,56 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-class TestFormulario:
+class TestCliente:
     def setup_method(self):
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
-        self.driver.get("https://vicent42.com/sistemas/ProyectoRM2/") 
+        self.driver.get("https://vicent42.com/sistemas/ProyectoRM2/Views/") 
         time.sleep(2)
     def test_iniciar_sesion(self):  
+        self.driver.get("https://vicent42.com/sistemas/ProyectoRM2/") 
+        time.sleep(2)
         self.driver.find_element(By.ID, "usuario").send_keys("admin")  
+        time.sleep(2)
         self.driver.find_element(By.ID, "clave").send_keys("admin")  
+        time.sleep(2)
         self.driver.find_element(By.XPATH, "//button[text()='Iniciar']").click()
-        time.sleep(3)
-    def test_ingreso_nuevo_cliente(self):
-        self.driver.get("https://vicent42.com/sistemas/ProyectoRM2/Clientes/Nuevo")  
-        time.sleep(2)  
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Ruc/Dni']").send_keys("12345678901")
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Nombre']").send_keys("Juan")
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Dirección']").send_keys("Calle Colon 123")
-        self.driver.find_element(By.XPATH, "//input[@placeholder='Teléfono']").send_keys("789456123")     
-        self.driver.find_element(By.XPATH, "//button[contains(text(),'Guardar')]").click()
-        time.sleep(3)
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//a[@href='https://vicent42.com/sistemas/ProyectoRM2/Clientes/Listar']").click()
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//button[@data-target='#nuevo_cliente']").click()
+        time.sleep(2) 
+        self.driver.find_element(By.XPATH, "//input[@id='ruc_insertar']").send_keys("12787")
+        time.sleep(2) 
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Nombre']").send_keys("Robertoooo to to to")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Dirección']").send_keys("Calle Colon")
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//input[@placeholder='Teléfono']").send_keys("78945619")     
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//button[@class='btn btn-primary' and @type='submit']").click()
+        time.sleep(2)
+    def test_verify_id(self):
+        esperado = "12787"
+        actual = self.driver.find_element(By.XPATH, "//app-persona//child::h4").text
+        assert esperado == actual, f"FAIL: Esperado: {esperado}, Actual: {actual}"
+        print(f"Verificación de ID exitosa. ID: {actual}")
+    def test_verify_name(self):
+        esperado = "Robertoooo to to to"  
+        actual = self.driver.find_element(By.XPATH, "//app-persona//child::h4").text
+        assert esperado == actual, f"FAIL: Esperado: {esperado}, Actual: {actual}"
+        print(f"Verificación de Nombre exitosa. Nombre: {actual}")
     def teardown_method(self):
         self.driver.quit()
         print("Prueba finalizada")
 if __name__ == "__main__":
-    test = TestFormulario()
+    test = TestCliente()
     test.setup_method()
     try:
         test.test_iniciar_sesion()
-        test.test_ingreso_nuevo_cliente()
+        test.test_verify_id()  
+        test.test_verify_name()  
     finally:
         test.teardown_method()

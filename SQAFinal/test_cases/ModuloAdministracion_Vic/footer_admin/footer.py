5cc6c6695ec94437123f4214d84ab124c9b6b86a
vic4284 <143749753+vic4284@users.noreply.github.com>
2024-12-04 10:54:33 -0400

    footer

---
+++ SQAFinal/test_cases/ModuloAdministracion_Vic/footer_admin/footer.py
@@ -0,0 +1,34 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+class Testfooter:
+    def setup_method(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.get('https://vicent42.com/sistemas/ProyectoRM2/')
+        time.sleep(3)
+
+    def teardown_method(self):
+        self.driver.quit()
+        print("Prueba de login completada")
+
+    def test_login(self):
+        # Llenar campos de usuario y contraseña
+        self.driver.find_element(By.ID, "usuario").send_keys("admin")
+        time.sleep(1)
+        self.driver.find_element(By.ID, "clave").send_keys("admin")
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[@class='btn btn-primary btn-block']").click()
+        time.sleep(3)
+
+        # Validar que se haya iniciado sesión correctamente
+        actual = self.driver.find_element(By.XPATH, "//div[text()='2024 © PASTELERIA ']").text 
+        esperado = "2024 © PASTELERIA ZAZA."
+        print(f"- VALOR ACTUAL = {actual}, - VALOR ESPERADO = {esperado}")
+        assert actual == esperado, f"Los valores no coinciden. ESPERADO: {esperado}, ACTUAL: {actual}"
+
+       
+    
